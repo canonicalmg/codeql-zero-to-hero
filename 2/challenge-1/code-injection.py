92edb727d986c605f00d5b24095dd9f1057c76d6
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-07 16:35:58 -0700

    [AUTO] Fix for This code execution depends on a [user-provided value](1).

--- 2/challenge-1/code-injection.py
+++ 2/challenge-1/code-injection.py
@@ -1,9 +1,18 @@
 from flask import Flask, request
+import ast
+import types
 app = Flask(__name__)
 
 @app.route("/code-execution")
 def code_execution():
     code = request.args.get("code")
-    exec(code) # NOT OK
-    eval(code) # NOT OK
-    
+    try:
+        parsed_code = ast.parse(code, mode='eval')
+        if isinstance(parsed_code, ast.Expression):
+            safe_code = compile(parsed_code, '<string>', 'eval')
+            result = eval(safe_code)
+        else:
+            raise ValueError("Invalid code")
+    except (SyntaxError, ValueError):
+        return "Error: Invalid code provided"
+    return str(result)

df1829d6f15524d89194e662df0c0751eb6f03a5
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-06 17:43:30 -0700

    [AUTO] Fix for This SQL query depends on a [user-provided value](1).

--- 2/challenge-1/sql-injection-flask.py
+++ 2/challenge-1/sql-injection-flask.py
@@ -7,17 +7,13 @@ class User(models.Model):
     pass
 
 @app.route("/users/<username>")
-def show_user():
-    username = request.args.get("username")
+def show_user(username):
     with connection.cursor() as cursor:
         # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
+        cursor.execute("SELECT * FROM users WHERE username = %s", [username])
         User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
 
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
-
-        # BAD -- other ways of executing raw SQL code with string interpolation
-        User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
-        User.objects.raw("insert into names_file ('name') values ('%s')" % username)
-        User.objects.extra("insert into names_file ('name') values ('%s')" % username)
+        # GOOD -- Using RawSQL with parameters
+        User.objects.annotate(RawSQL("insert into names_file ('name') values (%s)", (username,)))
+        User.objects.raw("insert into names_file ('name') values (%s)", (username,))
+        User.objects.extra("insert into names_file ('name') values (%s)", (username,))

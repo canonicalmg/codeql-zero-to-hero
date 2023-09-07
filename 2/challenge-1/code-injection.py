9651057302de53f1dfba8f034d30f042c86430dd
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-06 17:19:44 -0700

    [AUTO] Fix for This code execution depends on a [user-provided value](1).

--- 2/challenge-1/code-injection.py
+++ 2/challenge-1/code-injection.py
@@ -1,9 +1,1 @@
-from flask import Flask, request
-app = Flask(__name__)
-
-@app.route("/code-execution")
-def code_execution():
-    code = request.args.get("code")
-    exec(code) # NOT OK
-    eval(code) # NOT OK
-    
+N/A

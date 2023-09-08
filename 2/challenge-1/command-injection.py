ad4de5b27621c0b061a4ad7441beacdfa0745c1c
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-07 22:01:13 -0700

    [AUTO] Fixes for issues in 2/challenge-1/command-injection.py

--- 2/challenge-1/command-injection.py
+++ 2/challenge-1/command-injection.py
@@ -5,30 +5,22 @@ import subprocess
 from flask import Flask, request
 app = Flask(__name__)
 
+ALLOWED_FILES = ["file1", "file2", "file3"]
 
 @app.route("/command1")
 def command_injection1():
     files = request.args.get('files', '')
-    # Don't let files be `; rm -rf /`
-    os.system("ls " + files) # $result=BAD
-
+    if files in ALLOWED_FILES:
+        os.system("ls " + files) # $result=GOOD
 
 @app.route("/command2")
 def command_injection2():
     files = request.args.get('files', '')
-    # Don't let files be `; rm -rf /`
-    subprocess.Popen("ls " + files, shell=True) # $result=BAD
-
+    if files in ALLOWED_FILES:
+        subprocess.Popen("ls " + files, shell=True) # $result=GOOD
 
 @app.route("/path-exists-not-sanitizer")
 def path_exists_not_sanitizer():
-    """os.path.exists is not a sanitizer
-
-    This small example is inspired by real world code. Initially, it seems like a good
-    sanitizer. However, if you are able to create files, you can make the
-    `os.path.exists` check succeed, and still be able to run commands. An example is
-    using the filename `not-there || echo pwned`.
-    """
     path = request.args.get('path', '')
-    if os.path.exists(path):
-        os.system("ls " + path) # $result=BAD
+    if os.path.exists(path) and path in ALLOWED_FILES:
+        os.system("ls " + path) # $result=GOOD

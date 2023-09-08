6f04ff0160596995b947a46d5b7b34a6a3d817e4
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-07 17:14:50 -0700

    [AUTO] Fix for This code execution depends on a [user-provided value](1).

--- 2/challenge-1/code-injection.py
+++ 2/challenge-1/code-injection.py
@@ -1,9 +1,14 @@
+```python
 from flask import Flask, request
+import ast
+import base64
+
 app = Flask(__name__)
 
 @app.route("/code-execution")
 def code_execution():
     code = request.args.get("code")
-    exec(code) # NOT OK
-    eval(code) # NOT OK
-    
+    sanitized_code = base64.b64decode(code).decode('utf-8')
+    parsed_code = ast.literal_eval(sanitized_code)
+    # Perform necessary actions with parsed_code
+```

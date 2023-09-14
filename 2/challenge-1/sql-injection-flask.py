b344f83083a95fe26e125ebe70fcb15032651c10
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-09-14 16:35:38 -0700

    [AUTO] Fixes for issues in 2/challenge-1/sql-injection-flask.py

--- 2/challenge-1/sql-injection-flask.py
+++ 2/challenge-1/sql-injection-flask.py
@@ -11,13 +11,9 @@ def show_user():
     username = request.args.get("username")
     with connection.cursor() as cursor:
         # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
-        User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
+        cursor.execute("SELECT * FROM users WHERE username = %s", [username])
+        User.objects.raw("SELECT * FROM users WHERE username = %s", [username])
 
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+        # REMOVED -- BAD -- Using string formatting
 
-        # BAD -- other ways of executing raw SQL code with string interpolation
-        User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
-        User.objects.raw("insert into names_file ('name') values ('%s')" % username)
-        User.objects.extra("insert into names_file ('name') values ('%s')" % username)
+        # REMOVED -- BAD -- other ways of executing raw SQL code with string interpolation

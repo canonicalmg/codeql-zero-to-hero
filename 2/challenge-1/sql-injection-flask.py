9815b8c6bbcb2b9cd60a2058a5cd2fef8aec55b0
Marcus <10413185+canonicalmg@users.noreply.github.com>
2023-10-05 16:35:08 -0700

    [AUTO] Fixes for issues in 2/challenge-1/sql-injection-flask.py

--- 2/challenge-1/sql-injection-flask.py
+++ 2/challenge-1/sql-injection-flask.py
@@ -7,17 +7,12 @@ class User(models.Model):
     pass
 
 @app.route("/users/<username>")
-def show_user():
-    username = request.args.get("username")
+def show_user(username):
     with connection.cursor() as cursor:
         # GOOD -- Using parameters
-        cursor.execute("SELECT * FROM users WHERE username = %s", username)
-        User.objects.raw("SELECT * FROM users WHERE username = %s", (username,))
+        cursor.execute("SELECT * FROM users WHERE username = %s", [username])
+        User.objects.raw("SELECT * FROM users WHERE username = %s", [username])
 
-        # BAD -- Using string formatting
-        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+        # REMOVED -- BAD -- Using string formatting
 
-        # BAD -- other ways of executing raw SQL code with string interpolation
-        User.objects.annotate(RawSQL("insert into names_file ('name') values ('%s')" % username))
-        User.objects.raw("insert into names_file ('name') values ('%s')" % username)
-        User.objects.extra("insert into names_file ('name') values ('%s')" % username)
+        # REMOVED -- BAD -- other ways of executing raw SQL code with string interpolation
